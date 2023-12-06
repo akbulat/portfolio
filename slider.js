ef7b246f40a70c83771720f7d661996467394999
akbulat <59501229+akbulat@users.noreply.github.com>
2023-12-06 15:56:31 +0500

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,21 +1,23 @@
-let slideIndex = 0;
-showSlides(slideIndex);
+document.addEventListener('DOMContentLoaded', (event) => {
+    let slideIndex = 0;
+    showSlides(slideIndex);
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    if (n >= slides.length) slideIndex = 0;
-    if (n < 0) slideIndex = slides.length - 1;
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("slide");
+        if (n >= slides.length) slideIndex = 0;
+        if (n < 0) slideIndex = slides.length - 1;
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex].style.display = "block";
     }
-    slides[slideIndex].style.display = "block";
-}
 
-document.querySelector('.prev-slide').addEventListener('click', function() {
-    showSlides(--slideIndex);
-});
+    document.querySelector('.prev-slide').addEventListener('click', function() {
+        showSlides(--slideIndex);
+    });
 
-document.querySelector('.next-slide').addEventListener('click', function() {
-    showSlides(++slideIndex);
+    document.querySelector('.next-slide').addEventListener('click', function() {
+        showSlides(++slideIndex);
+    });
 });
